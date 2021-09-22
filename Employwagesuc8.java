d3c073e012cd40cb7de00f1622697da1ad22f1c8
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-09-22 15:33:45 +0530

    employ wage in mulyiple companies

---
+++ Employwagesuc8.java
@@ -0,0 +1,35 @@
+package Employwages;
+
+public class Employwagesuc8 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static int computeEmpWage(String company, int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+		//variables
+		int empHrs = 0, totalEmpHrs  = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= maxHoursPerMonth &&
+				totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
+			switch (empCheck ) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" + totalWorkingDays + "Emp Hr:" +empHrs);			
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total empwage for company:"+ company+"is:"+totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage("DMart", 20 , 2, 10);
+		computeEmpWage("Reliance", 10 , 4, 20);
+	}
+
+}
